13a7a24e5fe34e1c02c7d76b554dd66aec6f4fe2
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-09-20 15:06:02 -0500

    Basic folder implementing angular 2.0 version

--- app/services/common-interfaces.ts
+++
@@ -1,5 +0,0 @@
-import {ComparisonBarInput} from '../fe-core/components/comparison-bar/comparison-bar.component';
-
-export interface ComparisonBarList {
-  [year: string]: Array<ComparisonBarInput>;
-}

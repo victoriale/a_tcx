ac7f2f4ee2221892a97d792d51e78426ed61d6b0
victoriale <le.victoriavan@gmail.com>
2016-09-21 09:32:35 -0500

    add stuff

--- app/ngModules/global.ngmodule.ts
+++ app/ngModules/global.ngmodule.ts
@@ -2,18 +2,25 @@ import {CommonModule} from "@angular/common";
 import { NgModule }from '@angular/core';
 
 import { HeaderComponent } from "../fe-core/components/header/header.component";
-
+import { CircleImage } from "../fe-core/components/images/circle-image/circle-image";
+import { HoverImage } from '../fe-core/components/images/hover-image';
+import { ImageData } from '../fe-core/components/images/image-data';
 
 @NgModule({
     imports: [
       CommonModule
     ],
     declarations: [
-        HeaderComponent
+      HeaderComponent,
+      HoverImage,
+      ImageData
     ],
     exports:[
-        HeaderComponent
+      HeaderComponent,
+      HoverImage
     ],
-    providers: []
+    providers: [
+
+    ]
 })
 export class GlobalModule { }
--- app/webpages/deep-dive-page/deep-dive-page.ts
+++ app/webpages/deep-dive-page/deep-dive-page.ts
@@ -8,4 +8,25 @@ import {Component} from '@angular/core';
 
 export class DeepDivePage{
     title="Everything that is deep dive will go in this page. Please Change according to your requirement";
+    test: any = {
+              imageClass: "image-150",
+              mainImage: {
+                imageUrl: '/app/public/profile_placeholder.png',
+                urlRouteArray: ['/syndicated-article'],
+                hoverText: "<p>View</p> Profile",
+                imageClass: "border-large"
+              },
+              subImages: [
+                {
+                  imageUrl: '/app/public/profile_placeholder.png',
+                  urlRouteArray: ['/syndicated-article'],
+                  hoverText: "<i class='fa fa-mail-forward'></i>",
+                  imageClass: "image-50-sub image-round-lower-right"
+                },
+                {
+                  text: "#1",
+                  imageClass: "image-38-rank image-round-upper-left image-round-sub-text"
+                }
+              ],
+            }
 }

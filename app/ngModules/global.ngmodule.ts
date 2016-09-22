57d89aa8f0d101c61c2a6093a3a0b7f53fdac5a6
larryhpham <larrypham1991@yahoo.com>
2016-09-22 10:17:13 -0500

    added large-carousel for deepdive

--- app/ngModules/global.ngmodule.ts
+++ app/ngModules/global.ngmodule.ts
@@ -7,24 +7,37 @@ import { RectangleImage } from "../fe-core/components/images/rectangle-image/rec
 import { CircleImage } from "../fe-core/components/images/circle-image/circle-image";
 import { HoverImage } from "../fe-core/components/images/hover-image";
 import { FooterComponent } from "../fe-core/components/footer/footer.component";
+import { SideScroll } from "../fe-core/components/side-scroll/side-scroll.component";
+import {SanitizeStyle, SanitizeHtml, SanitizeRUrl} from '../fe-core/pipes/safe.pipe';
+
 @NgModule({
     imports: [
       CommonModule,
       routing
     ],
     declarations: [
+      //components
       HeaderComponent,
       HoverImage,
       CircleImage,
       RectangleImage,
-      FooterComponent
+      FooterComponent,
+
+      //modules
+      SideScroll,
+
+      //pipes
+      SanitizeStyle,
+      SanitizeHtml,
+      SanitizeRUrl
     ],
     exports:[
       HeaderComponent,
       HoverImage,
       CircleImage,
       RectangleImage,
-      FooterComponent
+      FooterComponent,
+      SideScroll
     ],
     providers: [
 

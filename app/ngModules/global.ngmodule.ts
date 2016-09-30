a43d6878818c8c209dde011b90f966b53f089fd1
victoriale <le.victoriavan@gmail.com>
2016-09-30 15:47:45 -0500

    section name

--- app/ngModules/global.ngmodule.ts
+++ app/ngModules/global.ngmodule.ts
@@ -9,27 +9,30 @@ import { VerticalGlobalFunctions } from "../global/vertical-global-functions";
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 import { GlobalSettings } from "../global/global-settings";
 import { GlobalFunctions } from "../global/global-functions";
-//components
-import { HeaderComponent } from "../fe-core/components/header/header.component";
-import { Search } from "../fe-core/components/search/search.component";
-import { HamburgerMenuComponent, MenuData } from '../fe-core/components/hamburger-menu/hamburger-menu.component';
-import { RectangleImage } from "../fe-core/components/images/rectangle-image/rectangle-image";
+/*** COMPONENTS ***/
 import { CircleImage } from "../fe-core/components/images/circle-image/circle-image";
+import { RectangleImage } from "../fe-core/components/images/rectangle-image/rectangle-image";
 import { HoverImage } from "../fe-core/components/images/hover-image";
-import { FooterComponent } from "../fe-core/components/footer/footer.component";
-import { ModuleHeader } from "../fe-core/components/module-header/module-header.component";
 import { ImagesMedia } from "../fe-core/components/carousels/images-media-carousel/images-media-carousel.component";
 import { CircleButton } from "../fe-core/components/buttons/circle/circle.button";
-import { LoadingComponent } from "../fe-core/components/loading/loading.component";
-import { CircleImageData } from "../fe-core/components/images/image-data";
-import { ScrollableContent } from "../fe-core/components/scrollable-content/scrollable-content.component";
+
+import { HeaderComponent } from "../fe-core/components/header/header.component";
+import { ModuleHeader } from "../fe-core/components/module-header/module-header.component";
+import { FooterComponent } from "../fe-core/components/footer/footer.component";
+import { HamburgerMenuComponent, MenuData } from '../fe-core/components/hamburger-menu/hamburger-menu.component';
 import { DropdownComponent } from "../fe-core/components/dropdown/dropdown.component";
-import { SearchBoxModule } from "../fe-core/modules/search-box-module/search-box-module.module";
-import { WidgetModule } from "../fe-core/modules/widget/widget.module";
-import { WidgetCarouselModule } from "../fe-core/modules/widget/widget-carousel.module";
+import { LoadingComponent } from "../fe-core/components/loading/loading.component";
+import { SectionNameComponent } from "../fe-core/components/section-name/section-name.component";
+import { Search } from "../fe-core/components/search/search.component";
+import { ScrollableContent } from "../fe-core/components/scrollable-content/scrollable-content.component";
 import { SidekickWrapperAI } from "../fe-core/components/sidekick-wrapper-ai/sidekick-wrapper-ai.component";
 import { Larousel } from '../fe-core/components/larousel/larousel';
 import { NewsBox } from '../fe-core/components/news-box/news-box';
+
+/*** MODULES ***/
+import { WidgetCarouselModule } from "../fe-core/modules/widget/widget-carousel.module";
+import { WidgetModule } from "../fe-core/modules/widget/widget.module";
+import { SearchBoxModule } from "../fe-core/modules/search-box-module/search-box-module.module";
 
 
 //Pipes
@@ -69,7 +72,8 @@ import { routing  } from '../app.routing';
       ScrollableContent,
       DropdownComponent,
 	    Larousel,
-      NewsBox
+      NewsBox,
+      SectionNameComponent
 
     ],
     exports: [
@@ -94,7 +98,8 @@ import { routing  } from '../app.routing';
       Larousel,
       DropdownComponent,
       Search,
-      NewsBox
+      NewsBox,
+      SectionNameComponent
     ],
     providers: [
       VerticalGlobalFunctions,

13c8b749b1eb67b7509e746c7604b516fb59f301
victoriale <le.victoriavan@gmail.com>
2016-10-06 12:51:31 -0500

    deep dive page update

--- app/webpages/deep-dive-page/deep-dive-page.ts
+++ app/webpages/deep-dive-page/deep-dive-page.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import { BoxScoresService } from '../../services/box-scores.service';
 import { SchedulesService } from '../../services/schedules.service';
 import { DeepDiveService } from '../../services/deep-dive.service';
 import { ActivatedRoute } from '@angular/router';
@@ -14,17 +13,12 @@ declare var jQuery: any;
 })
 
 export class DeepDivePage implements OnInit {
-    title = "Everything that is deep dive will go in this page. Please Change according to your requirement";
-    test: any = "testing";
     scope: string;
     carouselData: any;
-
     //side scroller
     sideScrollData: any;
     scrollLength: number = 0;
-    boxScoresTempVar: string = "nfl";
-
-    topScope: string = "weather";
+    topScope: string = "sports";
     changeScopeVar: string = "nfl";
     safeCall: boolean = true;
     ssMax: number;
@@ -32,47 +26,58 @@ export class DeepDivePage implements OnInit {
     callLimit: number = 25;
     scopeList: Array<string>;
     currentCategory:string;
-
     blockIndex: number = 1;
-
-    //Box Scores
-    boxScoresData: any;
-    currentBoxScores: any;
-    dateParam: any;
-
+    deepDiveType: string;
+    category: string;
     //Used for route subscription and unsubscribing when view is destroyed (double check since angular2 does it for you)
     routeSubscription:any;
 
-    constructor( private _boxScoresService: BoxScoresService, private _schedulesService:SchedulesService, private _deepDiveData: DeepDiveService, private _activatedRoute: ActivatedRoute) {
-      //Box Scores
-      var currentUnixDate = new Date().getTime();
-      //convert currentDate(users local time) to Unix and push it into boxScoresAPI as YYYY-MM-DD in EST using moment timezone (America/New_York)
-      this.dateParam ={
-        scope: this.boxScoresTempVar,//current profile page
-        teamId: '',
-        //date: '2016-09-22'
-        date: moment.tz( currentUnixDate , 'America/New_York' ).format('YYYY-MM-DD')
-      }
-    }
+    constructor(private _schedulesService:SchedulesService, private _deepDiveData: DeepDiveService, private _activatedRoute: ActivatedRoute) {}
 
-    ngOnInit() {
-      // this.scope = this.changeScopeVar ? this.changeScopeVar : this.topScope;
-      //TODO
+    ngOnInit(){
       this.routeSubscription = this._activatedRoute.params.subscribe(
           (param:any) => {
-            this.scope = param['category'];
+            this.category = param['category'];
+            this.scope = param['articleCategory'] ? param['articleCategory'] : param['category'];
             console.log('Partner:',GlobalSettings.getPartnerId());
             console.log('sectionFront parameters:',param);
           }
       );
-
-      this.getBoxScores(this.dateParam);
       this.getSideScroll();
       this.getDataCarousel();
+      this.deepDiveType = this.getDeepDiveType(this.category);
     }
 
     ngOnDestroy(){
       this.routeSubscription.unsubscribe();
+    }
+
+    private getDeepDiveType(category:string): string{
+      var _typeValue;
+      switch(category.toLowerCase()){
+        case 'sports':
+          _typeValue = "deep-dive-type1";
+          break;
+        case 'business':
+        case 'finance':
+        case 'real estate':
+        case 'weather':
+          _typeValue = "deep-dive-type2";
+          break;
+        case 'automotive':
+        case 'breaking news':
+        case 'entertainment':
+        case 'food':
+        case 'IPO':
+        case 'lifestyle':
+        case 'politics':
+        case 'travel':
+          _typeValue = "deep-dive-type3";
+          break;
+        default:
+          break;
+      }
+      return _typeValue;
     }
 
     private onScroll(event) {
@@ -160,17 +165,6 @@ export class DeepDivePage implements OnInit {
       this.getSideScroll();
     }
 
-    //api for Box Scores
-    private getBoxScores(dateParams?) {
-      // console.log('1. deep-dive-page, getBoxScores - dateParams - ',dateParams);
-      if ( dateParams != null ) {
-        this.dateParam = dateParams;
-      }
-      this._boxScoresService.getBoxScores(this.boxScoresData, this.boxScoresTempVar, this.dateParam, (boxScoresData, currentBoxScores) => {
-          this.boxScoresData = boxScoresData;
-          this.currentBoxScores = currentBoxScores;
-      });
-    }
 
     private getDataCarousel() {
       this._deepDiveData.getCarouselData('nfl', this.carouselData, '25', '1', 'CA', (carData)=>{

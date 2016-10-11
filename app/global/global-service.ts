6ada049500295dfb3fd40ee3d176a85b928b727e
victoriale <le.victoriavan@gmail.com>
2016-10-11 15:56:00 -0500

    geo location update

--- app/global/global-service.ts
+++ app/global/global-service.ts
@@ -10,41 +10,6 @@ import {Http, Headers} from '@angular/http';
 import {Observable} from "rxjs/Observable";
 import { GlobalFunctions } from "../global/global-functions";
 import { GlobalSettings } from "../global/global-settings";
-@Injectable()
-
-export class PartnerHeader {
-  public protocolToUse: string = (location.protocol == "https:") ? "https" : "http";
-
-  constructor(public http: Http) {}
-
-  //API for listing profile
-  getPartnerData(partner_id) {
-
-    // var partnerID = partner_id.split('-');
-    //
-    // //handles some cases where domain registries are different
-    // var combinedID = [];
-    // var domainRegisters = [];
-    // for(var i = 0; i < partnerID.length; i++){
-    //    if(partnerID[i] == "com" || partnerID[i] == "gov" || partnerID[i] == "net" || partnerID[i] == "org" || partnerID[i] == "co"){
-    //      combinedID.push(partnerID[i]);
-    //    }else{
-    //      domainRegisters.push(partnerID[i]);
-    //    }
-    // }
-    //
-    // partner_id = domainRegisters.join('-')+ "." + combinedID.join('.');
-
-    var fullUrl = GlobalSettings.getPartnerApiUrl(partner_id);
-    // console.log(fullUrl);
-    return this.http.get(fullUrl, {
-    })
-    .map(
-      res => res.json()
-    )
-    .map(data => {return data;})
-  }
-}
 
 @Injectable()
 export class GeoLocation {

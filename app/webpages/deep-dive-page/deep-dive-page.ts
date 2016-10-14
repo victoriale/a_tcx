b8a3e1fa60d35c0c31d11568a58f81ed0181e4d1
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2016-10-14 12:55:10 -0500

    quick fix

--- app/webpages/deep-dive-page/deep-dive-page.ts
+++ app/webpages/deep-dive-page/deep-dive-page.ts
@@ -142,7 +142,6 @@ export class DeepDivePage implements OnInit {
     }
 
     ngOnInit(){
-      GlobalSettings.getOffsiteLink("nfl", "team/seahawks/152");
       this.routeSubscription = this._activatedRoute.params.subscribe(
           (param:any) => {
             this.category = param['category'] ? param['category'] : 'all';

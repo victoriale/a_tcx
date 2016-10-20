38e473b768d2afea5bc77327c6a49dc7822e6015
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-10-20 16:03:35 -0500

    search-module changes

--- app/global/global-settings.ts
+++ app/global/global-settings.ts
@@ -508,7 +508,7 @@ export class GlobalSettings {
           }
           else {
 
-            link = "http://investkit.com"+ "/" + relativeUrl;
+            link = "http://investkit.com/" + relativeUrl;
 
           }
           break;

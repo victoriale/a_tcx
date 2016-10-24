39a1a4215ec58c50fea560ace250076036e75617
Navyaeetaram <navya.eataram@sntmedia.com>
2016-10-24 13:38:32 -0500

    remove consolelogs

--- app/webpages/syndicated-article-page/syndicated-article-page.ts
+++ app/webpages/syndicated-article-page/syndicated-article-page.ts
@@ -113,8 +113,6 @@ export class SyndicatedArticlePage implements OnChanges,OnDestroy{
         this._synservice.getSyndicateVideoService(articleID).subscribe(
             data => {
                 this.articleData = data.data;
-                console.log(this.articleData);
-
                 this.iframeUrl = this.articleData.video_url + "&autoplay=on";
             }
         )

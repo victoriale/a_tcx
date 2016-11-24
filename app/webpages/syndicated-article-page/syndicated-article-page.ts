12cbadc5beacbcfbc442a7498482daa43a42b1bf
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-11-23 21:03:44 -0600

    changes

--- app/webpages/syndicated-article-page/syndicated-article-page.ts
+++ app/webpages/syndicated-article-page/syndicated-article-page.ts
@@ -3,7 +3,7 @@ import {SyndicateArticleService} from "../../services/syndicate-article.service"
 import {GlobalSettings} from "../../global/global-settings";
 import {ActivatedRoute, Route, Router, NavigationStart, Event as NavigationEvent} from "@angular/router";
 import {GlobalFunctions} from "../../global/global-functions";
-import {win} from "@angular/platform-browser/src/facade/browser";
+cd app/fe
 
 declare var jQuery:any;
 declare var moment;

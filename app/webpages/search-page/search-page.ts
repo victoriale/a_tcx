33dac2b46bc417313c99eed7e8127d781c9e1079
Navyaeetaram <navya.eetaram@sntmedia.com>
2016-12-12 16:13:26 -0600

    search results

--- app/webpages/search-page/search-page.ts
+++ app/webpages/search-page/search-page.ts
@@ -1,5 +1,5 @@
 
-import {Component} from "@angular/core";
+import {Component, HostListener, Renderer} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {SearchService} from "../../services/search.service";
 @Component({
@@ -15,12 +15,12 @@ export class SearchPage{
     currentPage:number=1;
     pageCount:number=5;
     articleCount:number=this.pageCount*10;
-    constructor(private activateRoute:ActivatedRoute, private searchService:SearchService){
+    public scrollTopPrev:number=0;
+    constructor(private activateRoute:ActivatedRoute, private searchService:SearchService, private _render:Renderer){
         this.paramsub=activateRoute.params.subscribe(
             (param :any)=> {
                 this.userInput= param['userInput'];
-                //this.getSearchResult(this.userInput,currentPage);
-                this.getDummySearchResults(this.currentPage);
+                this.getSearchResult(this.userInput,this.currentPage);
             }
         );
 
@@ -32,19 +32,58 @@ export class SearchPage{
     private getSearchResult(i,currentPage){
         this.searchService.searchArticleService(i,currentPage).subscribe(
             data=>{
-                this.searchArticlesData=this.searchService.transformSearchResults(data);
-                console.log(this.searchArticlesData);
+                if(data.data.article_data) {
+                    this.searchArticlesData = this.searchService.transformSearchResults(data.data);
+                }else{
+                    this.searchArticlesData=null;
+                }
             }
         )
     }
-    private getDummySearchResults(currentPage){
-      this.searchArticlesData=this.searchService.getdummydata(currentPage);
-    }
+
     ngOnDestroy(){
         this.paramsub.unsubscribe();
     }
     getPageOnClick(e){
         this.currentPage=e;
-        this.getDummySearchResults(this.currentPage);
+        this.getSearchResult(this.userInput,this.currentPage);
     }
+    @HostListener('window:scroll',['$event']) onScroll(e){
+        var scrollingElement=e.target.body.getElementsByClassName('search-widget')[0];
+        var header = e.target.body.getElementsByClassName('header')[0];
+        var articleContainer= e.target.body.getElementsByClassName('search-article-container')[0];
+        var fixedHeader = e.target.body.getElementsByClassName('fixedHeader')[0] != null ? e.target.body.getElementsByClassName('fixedHeader')[0].offsetHeight : 0;
+        var footer = e.target.body.getElementsByClassName('footer')[0];
+        let topCSS = 0;
+        topCSS = header != null ? topCSS + header.offsetHeight : topCSS;
+        topCSS = articleContainer != null ? topCSS +  articleContainer.offsetHeight : topCSS;
+        topCSS = topCSS - fixedHeader;
+        let bottomCSS=0;
+        bottomCSS = footer!=null? bottomCSS + footer.offsetHeight: bottomCSS;
+        var scrollTop = e.srcElement.body.scrollTop;
+        let scrollUp = scrollTop - this.scrollTopPrev>0?true:false;
+        var scrollBottom = e.target.body.scrollHeight-e.target.body.scrollTop==e.target.body.clientHeight?true:false;
+        this.scrollTopPrev=scrollTop;
+        if(scrollingElement){
+            if(window.scrollY > topCSS){
+                if(scrollUp) {
+                    var sctop = window.scrollY - topCSS - 25 + 'px';
+                    this._render.setElementStyle(scrollingElement, 'top', sctop);
+                }else{
+                    var headerTop=e.target.body.getElementsByClassName('header-top')[0];
+                    var partnerheadTop=document.getElementById('partner_header')?document.getElementById('partner_header').offsetHeight:0;
+                    var sctop = headerTop.offsetHeight? window.scrollY - topCSS + headerTop.offsetHeight + partnerheadTop + 10 + 'px' :window.scrollY - topCSS + partnerheadTop + 'px';
+                    this._render.setElementStyle(scrollingElement, 'top', sctop);
+                }
+                if(scrollBottom && window.innerHeight - footer.offsetHeight <600){
+                    var newTopCSS = window.scrollY - topCSS - bottomCSS - 50+ 'px';
+                    this._render.setElementStyle(scrollingElement,'top', newTopCSS);
+                }
+            }else {
+                this._render.setElementStyle(scrollingElement, "top", '0')
+            }
+
+        }
+    }
+
 }
